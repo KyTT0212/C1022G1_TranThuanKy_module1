557d801601521ead6dfaf39c1ce14b5dfc561e51
trankyy0212 <tranthuanky@gmail.com>
2023-02-14 16:35:25 +0700

    bt

---
+++ module3_ss12/thuc_hanh/src/main/java/repository/impl/UserRepository.java
@@ -0,0 +1,58 @@
+package repository.impl;
+
+import model.User;
+import repository.IUserRepository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserRepository implements IUserRepository {
+    private static String jdbcURL = "jdbc:mysql://localhost:3306/BDName";
+    private static String jdbcUsername = "root";
+    private static String jdbcPassword = "12345678";
+    private static Connection connection;
+
+    private static final String INSERT_USERS_SQL = "INSERT INTO users (name, email, country) VALUES (?, ?, ?);";
+    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
+    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    @Override
+    public void insertUser(User user) throws SQLException {
+
+    }
+
+    @Override
+    public User selectUser(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return null;
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        return false;
+    }
+}
